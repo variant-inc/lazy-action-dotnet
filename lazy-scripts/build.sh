798c3d8a7992f1af744229e97ac9bd56a3c70a73
Naveen Paka <npaka@usxvar-m3l6ml85.lan>
2021-01-22 00:10:16 -0500

    initial files

---
+++ lazy-scripts/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -l
+set -euo pipefail
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: ./build.sh [REPO_NAME] [IMAGE_NAME] [IMAGE_TAG]"
+    exit 1
+fi
+
+DOCKER_REPO_NAME=${1}
+IMAGE_NAME=${2}
+IMAGE_TAG=${3}
+IMAGE=${DOCKER_REPO_NAME}/${IMAGE_NAME}:${IMAGE_TAG}
+
+function cleanup()
+{
+  set +euo pipefail
+  docker image rm ${IMAGE}
+}
+
+trap "cleanup" EXIT
+
+docker build -t ${IMAGE} --build-arg "GITHUB_USER" --build-arg "GITHUB_TOKEN" .
+#curl -s https://ci-tools.anchore.io/inline_scan-latest | bash -s -- -r -f -d Dockerfile ${IMAGE}
+echo "::set-output name=image::${IMAGE}"
