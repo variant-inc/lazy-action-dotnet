aa3c85070911b58b138799909bd8d0724f338494
Vibin Daniel <vibind@drivevariant.com>
2021-01-29 19:32:43 -0500

    lint fixes

--- scripts/docker_sonarscan.sh
+++ scripts/docker_sonarscan.sh
@@ -1,18 +1,18 @@
 #!/bin/sh -l
 
-set -euo pipefail
+set -euo
 
-function cleanup() {
+cleanup() {
   set +euo pipefail
   docker image rm sonar
 }
 
 trap "cleanup" EXIT
 
-docker build --target sonarscan-env -t ${SONARSCAN_IMAGE} --build-arg "GITHUB_USER" --build-arg "GITHUB_TOKEN" .
-args="-e SONAR_TOKEN ${SONARSCAN_IMAGE} -d testresults -o ${SONAR_ORG} -k ${SONAR_PROJECT_KEY} -r ${GITHUB_SHA}"
+docker build --target sonarscan-env -t "$SONARSCAN_IMAGE" --build-arg "$GITHUB_USER" --build-arg "$GITHUB_TOKEN" .
+args="-e SONAR_TOKEN $SONARSCAN_IMAGE -d testresults -o $SONAR_ORG -k $SONAR_PROJECT_KEY -r $GITHUB_SHA"
 if [ -z ${PULL_REQUEST_KEY} ]; then
-  docker run --rm $args -b ${BRANCH_NAME}
+  docker run --rm "$args" -b "$BRANCH_NAME"
 else
-  docker run --rm $args -p ${PULL_REQUEST_KEY}
+  docker run --rm "$args" -p "$PULL_REQUEST_KEY"
 fi
--- scripts/sonarscan.sh
+++ scripts/sonarscan.sh
@@ -3,29 +3,28 @@
 set -eu
 
 OUTPUTDIR="./coverage"
-mkdir -p $OUTPUTDIR
+mkdir -p "$OUTPUTDIR"
 
-SONAR_ORGANIZATION=$SONAR_ORG
-REVISION=$GITHUB_SHA
+SONAR_ORGANIZATION="$SONAR_ORG"
 
-function sonar_logout() {
+sonar_logout() {
   set +eu
-  dotnet sonarscanner end /d:sonar.login=${SONAR_TOKEN}
+  dotnet sonarscanner end /d:sonar.login="$SONAR_TOKEN"
 }
 
-sonar_args="/o:${SONAR_ORGANIZATION} \
-    /k:${SONAR_PROJECT_KEY} \
+sonar_args="/o:$SONAR_ORGANIZATION \
+    /k:$SONAR_PROJECT_KEY \
     /d:sonar.host.url="https://sonarcloud.io" \
-    /d:sonar.login=${SONAR_TOKEN} \
+    /d:sonar.login=$SONAR_TOKEN \
     /d:sonar.cs.opencover.reportsPaths=**/coverage.opencover.xml \
     /d:sonar.exclusions=**/*Migrations/**/* \
     /d:sonar.scm.disabled=true \
-    /d:sonar.scm.revision=${REVISION}"
+    /d:sonar.scm.revision=$GITHUB_SHA"
 
-if [ -z ${PULL_REQUEST_KEY} ]; then
-  dotnet sonarscanner begin $sonar_args /d:sonar.branch.name=${BRANCH_NAME}
+if [ -z "$PULL_REQUEST_KEY" ]; then
+  dotnet sonarscanner begin "$sonar_args" /d:sonar.branch.name="$BRANCH_NAME"
 else
-  dotnet sonarscanner begin $sonar_args /d:sonar.pullrequest.key=${PULL_REQUEST_KEY}
+  dotnet sonarscanner begin "$sonar_args" /d:sonar.pullrequest.key="$PULL_REQUEST_KEY"
 fi
 
 trap "sonar_logout" EXIT
@@ -34,12 +33,12 @@ trap "sonar_logout" EXIT
 set +e
 dotnet test \
   /p:CollectCoverage=true \
-  /p:CoverletOutput=${OUTPUTDIR}/coverage.opencover.xml \
+  /p:CoverletOutput="$OUTPUTDIR/coverage.opencover.xml" \
   /p:CoverletOutputFormat=opencover \
   /p:CoverletSkipAutoProps=true \
-  /p:Exclude=[*]*Migrations.*
+  /p:Exclude="[*]*Migrations.*"
 set -e
 
 dotnet reportgenerator \
-  -reports:**/${OUTPUTDIR}/coverage.opencover.xml \
-  -targetdir:${OUTPUTDIR}
+  -reports:"**/$OUTPUTDIR/coverage.opencover.xml" \
+  -targetdir:"$OUTPUTDIR"
