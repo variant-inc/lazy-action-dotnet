3eb241184d2e6e643915fd27fe0c05ab02837a7f
Vibin Daniel <vibind@drivevariant.com>
2021-01-29 19:07:53 -0500

    cleanup

---
+++ scripts/coverage_scan.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -l
+
+set -euo pipefail
+
+pull_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+export PULL_REQUEST_KEY=$pull_number
+
+if [ -z "${INPUT_DOCKERFILE_DIR_PATH}" ]; then
+  echo "running lazy sonar tests."
+  /scripts/sonarscan.sh
+else
+  /scripts/docker_sonarscan.sh
+fi
