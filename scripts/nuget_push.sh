bea653ac5104b2a1f5f2fb94d689bc4b4cd47bf8
Naveen Paka <npaka@usxvar-m3l6ml85.lan>
2021-02-01 15:46:25 -0500

    Adding some corrections of nuget packaging

--- scripts/nuget_push.sh
+++ scripts/nuget_push.sh
@@ -8,11 +8,12 @@ cd "$GITHUB_WORKSPACE"
 
 mkdir -p out
 
-dotnet pack "$INPUT_NUGET_SRC_PROJECT" --no-restore -c Release --version-suffix "$IMAGE_VERSION" -o /out
+
 if [ -n "$INPUT_NUGET_PACKAGE_NAME" ]; then
-  dotnet nuget push "/out/**/*.nupkg" --source github --skip-duplicate --api-key "$GITHUB_TOKEN" -p:PackageID="$INPUT_NUGET_PACKAGE_NAME"
+  dotnet pack "$INPUT_NUGET_SRC_PROJECT" --no-restore -c Release --version-suffix "$IMAGE_VERSION" -o /out -p:PackageID="$INPUT_NUGET_PACKAGE_NAME"
 else
-  dotnet nuget push "/out/**/*.nupkg" --source github --skip-duplicate --api-key "$GITHUB_TOKEN"
+  dotnet pack "$INPUT_NUGET_SRC_PROJECT" --no-restore -c Release --version-suffix "$IMAGE_VERSION" -o /out
 fi
+dotnet nuget push "/out/**/*.nupkg" --source github --skip-duplicate --api-key "$GITHUB_TOKEN"
 
 rm -rf out
