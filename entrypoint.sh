8b0764abdf7a985be7d3d38f9d3b559a302469a9
Vibin Daniel <vibind@drivevariant.com>
2021-02-01 09:54:50 -0500

    pass all env as build args
    logs
    
    
    fix docker build
    
    
    use eval

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,6 @@ export BRANCH_NAME="$GITVERSION_BRANCHNAME"
 echo "Print Branch name: $BRANCH_NAME"
 
 export GITHUB_USER="$GITHUB_REPOSITORY_OWNER"
-export GITHUB_TOKEN="${NUGET_AUTH_TOKEN:-$GITHUB_TOKEN}"
 
 echo "End: Setting Prerequisites"
 
