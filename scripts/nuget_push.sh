cabd51e7fc2d90aba8216111fc2fe4fef05d6031
Naveen Paka <npaka@usxvar-m3l6ml85.lan>
2021-02-02 10:55:07 -0500

    adding updated read me file
    Updated read me file with more info
    
    Updated read me file with more info
    
    Updated read me file with more info
    
    Updated read me file with more info
    
    Updated read me file with more info
    
    Updated read me file with more info

--- scripts/nuget_push.sh
+++ scripts/nuget_push.sh
@@ -10,6 +10,7 @@ mkdir -p out
 
 
 if [ -n "$INPUT_NUGET_PACKAGE_NAME" ]; then
+  echo "Taking nuget pacakge name"
   dotnet pack "$INPUT_NUGET_SRC_PROJECT" --no-restore -c Release --version-suffix "$IMAGE_VERSION" -o /out -p:PackageID="$INPUT_NUGET_PACKAGE_NAME"
 else
   dotnet pack "$INPUT_NUGET_SRC_PROJECT" --no-restore -c Release --version-suffix "$IMAGE_VERSION" -o /out
