a7867ce2c39cc33ad197ebbb7a69b96afd1480a7
Naveen Paka <npaka@usxvar-m3l6ml85.lan>
2021-02-02 14:08:06 -0500

    Adding linter corrections
    Adding linter corrections
    
    Adding linter corrections
    
    Adding linter corrections

--- scripts/docker_sonarscan.sh
+++ scripts/docker_sonarscan.sh
@@ -8,7 +8,6 @@ cleanup() {
 }
 
 trap "cleanup" EXIT
-
 eval "docker build --target $INPUT_SONAR_SCAN_IN_DOCKER_TARGET -t sonarscan env | cut -f1 -d= | sed 's/^/--build-arg /' ."
 args="-e SONAR_TOKEN sonarscan -d testresults -o $SONAR_ORG -k $SONAR_PROJECT_KEY -r $GITHUB_SHA"
 if [ -z "$PULL_REQUEST_KEY" ]; then
