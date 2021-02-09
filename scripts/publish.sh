03d69ac3a9ca750a36bc321c6bc951959496da89
Vibin Daniel <vibind@drivevariant.com>
2021-02-09 17:13:22 -0500

    Update publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 ECR_REGISTRY="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
