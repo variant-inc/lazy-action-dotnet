395e56619b113140ca5e0271dccc98d21957c79e
Naveen Paka <npaka@usxvar-m3l6ml85.lan>
2021-02-11 16:52:05 -0500

    build fix

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -29,7 +29,9 @@ if [ -z "$DOCKERFILE_PATH" ]; then
   dotnet publish -c Release -o publish
 fi
 
-docker build $(echo $(env | cut -f1 -d= | sed 's/^/--build-arg /')) -t "$IMAGE" "$DOCKERFILE_PATH"
+#docker build $(echo $(env | cut -f1 -d= | sed 's/^/--build-arg /')) -t "$IMAGE" "$DOCKERFILE_PATH"
+
+eval "docker build -t $IMAGE $DOCKERFILE_PATH $(for i in $(env); do out+="--build-arg $i "; done; echo "$out")"
 
 docker push "$IMAGE"
 echo "Image publish done"
