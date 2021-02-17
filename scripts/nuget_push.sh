f89e2903df9f056356c9018c99af7774025624da
Hitesh Patel <HiteshP@drivevariant.com>
2021-02-17 13:46:22 -0500

    NuGet changes

--- scripts/nuget_push.sh
+++ scripts/nuget_push.sh
@@ -2,19 +2,11 @@
 
 set -eu
 
-: "$INPUT_NUGET_PROJECT_PATH"
-
 cd "$GITHUB_WORKSPACE"
 
 mkdir -p out
 
-
-if [ -n "$INPUT_NUGET_PACKAGE_NAME" ]; then
-  echo "Taking nuget pacakge name"
-  dotnet pack "$INPUT_NUGET_PROJECT_PATH" --no-restore -c Release --version-suffix "$IMAGE_VERSION" -o /out -p:PackageID="$INPUT_NUGET_PACKAGE_NAME"
-else
-  dotnet pack "$INPUT_NUGET_PROJECT_PATH" --no-restore -c Release --version-suffix "$IMAGE_VERSION" -o /out
-fi
-dotnet nuget push "/out/**/*.nupkg" --source github --skip-duplicate --api-key "$GITHUB_TOKEN"
+dotnet pack --no-restore -c Release --version-suffix "$IMAGE_VERSION" -o /out
+dotnet nuget push "/out/**/*.nupkg" --source github --skip-duplicate --api-key "$INPUT_NUGET_PUSH_TOKEN"
 
 rm -rf out
