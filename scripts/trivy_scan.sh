7d4ceabbe96c0b8e68169873a54dd255249f43bd
Naveen Paka <naveenp@drivevariant.com>
2021-03-08 16:34:08 -0500

    Added Account id to image

--- scripts/trivy_scan.sh
+++ scripts/trivy_scan.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "Checking repo trivy folder exists."
+set -e
+
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 ECR_REGISTRY="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
 IMAGE="$ECR_REGISTRY/$INPUT_ECR_REPOSITORY:$IMAGE_VERSION"
 
