ae5b66566551d7da04296ec776ac7cf8cf3cec4a
Naveen Paka <naveenp@drivevariant.com>
2021-03-08 16:20:21 -0500

    Corrected ECR image tag

--- scripts/trivy_scan.sh
+++ scripts/trivy_scan.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Checking repo trivy folder exists."
+ECR_REGISTRY="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
 IMAGE="$ECR_REGISTRY/$INPUT_ECR_REPOSITORY:$IMAGE_VERSION"
 
 trivy $IMAGE
