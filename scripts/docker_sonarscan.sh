5c0b43b9004eeb6c9caba283e86cdbbc6d0fa440
John Kane <johnk@drivevariant.com>
2021-03-11 11:35:20 -0500

    set prerelease commit

--- scripts/docker_sonarscan.sh
+++ scripts/docker_sonarscan.sh
@@ -10,7 +10,7 @@ cleanup() {
 trap "cleanup" EXIT
 eval "docker build --target $INPUT_SONAR_SCAN_IN_DOCKER_TARGET -t sonarscan . $(for i in $(env); do out+="--build-arg $i "; done; echo "$out")"
 args=("-d testresults" "-o $SONAR_ORG" "-k $SONAR_PROJECT_KEY" "-r $GITHUB_SHA")
-if [ -z "$PULL_REQUEST_KEY" ]; then
+if [ "$PULL_REQUEST_KEY" = null ]; then
     args+=("-b $BRANCH_NAME")
 else
     args+=("-p $PULL_REQUEST_KEY")
