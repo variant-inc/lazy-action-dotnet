b96cd822d2e1a974db7926f8f55bada00e487c41
Naveen Paka <naveenp@drivevariant.com>
2021-03-24 13:38:35 -0400

    Added local changes to branch

--- scripts/trivy_scan.sh
+++ scripts/trivy_scan.sh
@@ -7,5 +7,16 @@ AWS_ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 ECR_REGISTRY="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
 IMAGE="$ECR_REGISTRY/$INPUT_ECR_REPOSITORY:$IMAGE_VERSION"
 
+aws s3 cp s3://trivy-root/.trivyignore .trivyignore
+
+if [ -d "$GITHUB_WORKSPACE/trivy" ] 
+then
+    echo "Directory trivy exists." 
+    cat $GITHUB_WORKSPACE/trivy/.trivyignore >> .trivyignore
+else
+    echo "Error: Directory $GITHUB_WORKSPACE/trivy does not exists."
+fi
+
+
 trivy $IMAGE
 
