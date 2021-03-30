b0b194b44c82e6ba7c65cee8b4c53307d80aba17
Naveen Paka <naveenp@drivevariant.com>
2021-03-30 15:40:30 -0400

    Changed logic to read file from bucket folders

--- scripts/trivy_scan.sh
+++ scripts/trivy_scan.sh
@@ -6,6 +6,7 @@ set -e
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 ECR_REGISTRY="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
 IMAGE="$ECR_REGISTRY/$INPUT_ECR_REPOSITORY:$IMAGE_VERSION"
+S3_BUCKET_NAME="trivy-ops"
 
 credentials=$(aws sts assume-role --role-arn arn:aws:iam::108141096600:role/ops-github-runner --role-session-name ops-s3)
 
@@ -15,18 +16,18 @@ aws configure set aws_secret_access_key $(echo "$credentials" | jq -r '.Credenti
 aws configure set aws_session_token $(echo "$credentials" | jq -r '.Credentials.SessionToken') --profile ops
 
 echo "Download trivy file from s3." 
-aws --profile s3 cp s3://trivy-ops/.trivyignore .
+aws --profile ops s3 cp s3://${S3_BUCKET_NAME}/.trivyignore .
 
 echo "Print repo name: $GITHUB_REPOSITORY"
-
-if [[ ! -z $(aws s3api list-buckets --query 'Buckets[?Name==`'$GITHUB_REPOSITORY'`]' --output text) ]]; 
-then
-  echo "Repo bucket exists."
-   mkdir trivy
-   cd trivy && aws --profile s3 cp s3://$GITHUB_REPOSITORY/.trivyignore .
-   cat $GITHUB_WORKSPACE/trivy/.trivyignore >> .trivyignore
+PATH_TO_FOLDER=$GITHUB_REPOSITORY
+totalFoundObjects=$(aws s3 --profile ops ls s3://${S3_BUCKET_NAME}/${PATH_TO_FOLDER}/.trivyignore --recursive --summarize | grep "Total Objects: " | sed 's/[^0-9]*//g')
+if [ "$totalFoundObjects" -eq "0" ]; then
+   echo "There are no repo files found"
 else
-  echo "Repo bucket does not exists."
+  echo "Repo file found $totalFoundObjects"
+  mkdir trivy
+  cd trivy && aws --profile s3 cp s3://${S3_BUCKET_NAME}/${PATH_TO_FOLDER}/.trivyignore .
+  cat $GITHUB_WORKSPACE/trivy/.trivyignore >> .trivyignore
 fi
 
 cat .trivyignore
