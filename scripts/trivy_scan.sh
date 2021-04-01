713b4710a09713f3b22a6ac33904f2ef6004bd33
Naveen <naveenp@drivevariant.com>
2021-04-01 16:55:14 -0400

    Adding back all commented changes

--- scripts/trivy_scan.sh
+++ scripts/trivy_scan.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "Checking repo trivy folder exists."
 set -e
 
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
@@ -16,7 +15,7 @@ aws configure set aws_secret_access_key $(echo "$credentials" | jq -r '.Credenti
 aws configure set aws_session_token $(echo "$credentials" | jq -r '.Credentials.SessionToken') --profile ops
 
 echo "Print repo name: $GITHUB_REPOSITORY"
-echo "Download trivy file from s3." 
+echo "Download root trivy file from s3" 
 aws --profile ops s3 cp s3://${S3_BUCKET_NAME}/.trivyignore .
 
 PATH_TO_FOLDER=$GITHUB_REPOSITORY
@@ -27,13 +26,13 @@ else
   echo "Repo file found $totalFoundObjects"
   mkdir trivy
   cd trivy && aws --profile ops s3 cp s3://${S3_BUCKET_NAME}/${PATH_TO_FOLDER}/.trivyignore .
-  echo "Downloaded from specific repo."
-  pwd && ls -a
+  echo "Downloaded from specific repo"
   cd "$GITHUB_WORKSPACE" && cat trivy/.trivyignore >> .trivyignore
 fi
 
+echo "Printing trivy ignore file" 
 cat .trivyignore
 
-trivy --severity=CRITICAL,HIGH,MEDIUM --exit-code 1 $IMAGE
+trivy --exit-code 1 $IMAGE
 
 
