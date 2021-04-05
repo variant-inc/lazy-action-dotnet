b20c468f6bf0e63f61f2b801371837fa51125a33
Naveen <naveenp@drivevariant.com>
2021-04-05 15:36:22 -0400

    Changing back code
    Adding refactoring final commit

--- scripts/trivy_scan.sh
+++ scripts/trivy_scan.sh
@@ -8,7 +8,6 @@ IMAGE="$ECR_REGISTRY/$INPUT_ECR_REPOSITORY:$IMAGE_VERSION"
 
 echo "Building docker image"
 eval "docker build -t $IMAGE $INPUT_DOCKERFILE_DIR_PATH $(for i in $(env); do out+="--build-arg $i "; done; echo "$out")"
-S3_BUCKET_NAME=trivy-ops
 
 credentials=$(aws sts assume-role --role-arn arn:aws:iam::108141096600:role/ops-github-runner --role-session-name ops-s3)
 
@@ -21,8 +20,8 @@ echo "Print repo name: $GITHUB_REPOSITORY"
 echo "Download root trivy file from s3" 
 eval "aws --profile ops s3 cp s3://${S3_BUCKET_NAME}/.trivyignore ."
 
+S3_BUCKET_NAME=trivy-ops
 PATH_TO_FOLDER=$GITHUB_REPOSITORY
-PATH_TO_FOLDER=variant-inc/demo-app1
 mkdir trivy
 echo "Checking repo trivy file from s3"
 
